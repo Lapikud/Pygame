506a902f0cd63ee97b4145e1c26da8670a619ee4
Laura Anni <laanni@ttu.ee>
2023-03-11 20:29:07 +0200

    Initial setup

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+# create a window
+screen = pygame.display.set_mode((1200, 600))
+
+running = True
+while running:
+
+    # Events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # closing window finishes game loop
+            running = False
+
+    # Draw
+
+    # Update screen
+    pygame.display.flip()
