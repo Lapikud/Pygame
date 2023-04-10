efdfd38b47c70c9b5f94cff825b0795edcb4324b
Andree Uuetoa <andree.uuetoa@gmail.com>
2023-04-10 23:30:21 +0300

    Fix adding object to gate

--- gate.py
+++ gate.py
@@ -6,7 +6,7 @@ class Gate:
         self.objects = []
 
     def add_object(self, game_object):
-        self.objects.append(object)
+        self.objects.append(game_object)
 
     def get_object(self):
         if len(self.objects) > 0:
